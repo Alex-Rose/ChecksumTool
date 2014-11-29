4efd566786cbc37423a1e622542d4c05f0addfcd
Alexandre Rose <alexandre.rose.0@gmail.com>
2014-11-29 13:25:31 -0500

    Created base system

---
+++ ChecksumTool/Checksum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChecksumTool
+{
+    public enum HashAlgorithm
+    {
+        SHA1 = 0,
+        MD5,
+    }
+
+    public static class Checksum
+    {
+        public static string GetChecksum(HashAlgorithm algo, String[] files)
+        {
+            byte[] hash = new byte[]{};
+            switch(algo)
+            {
+                case HashAlgorithm.SHA1:
+                    hash = ComputeSha1(files);
+                    break;
+                case HashAlgorithm.MD5:
+
+                    break;
+            }
+
+            var strHash = BitConverter.ToString(hash);
+            return strHash.Replace("-", "").ToLower();
+        }
+
+        private static byte[] ComputeSha1(String[] files)
+        {
+            var file = files[0];
+            file = file.Trim('"');
+            var buffer = File.ReadAllBytes(file);
+            var hash = SHA1.Create().ComputeHash(buffer);
+
+            return hash;
+        }
+    }
+}
---
+++ ChecksumTool/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChecksumTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public string FilePath
+        {
+            get { return (string)GetValue(FilePathProperty); }
+            set { SetValue(FilePathProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FilePath.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilePathProperty =
+            DependencyProperty.Register("FilePath", typeof(string), typeof(MainWindow));
+
+
+
+        public string Hash
+        {
+            get { return (string)GetValue(HashProperty); }
+            set { SetValue(HashProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Hash.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HashProperty =
+            DependencyProperty.Register("Hash", typeof(string), typeof(MainWindow));
+
+
+
+        public string SourceHash
+        {
+            get { return (string)GetValue(SourceHashProperty); }
+            set { SetValue(SourceHashProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ComprareHash.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SourceHashProperty =
+            DependencyProperty.Register("SourceHash", typeof(string), typeof(MainWindow));
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            Hash = "Please select a file";
+            FilePath = @"C:\Users\Alexandre\Downloads\gpg4win-2.2.3.exe";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Hash = Checksum.GetChecksum(HashAlgorithm.SHA1, new[] {FilePath});
+            CompareHash();
+        }
+
+        private void TextBox_Drop(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void TextBox_PreviewDrop(object sender, DragEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Grid_Drop(object sender, DragEventArgs e)
+        {
+            var files = e.Data.GetData(DataFormats.FileDrop) as IEnumerable<string>;
+            FilePath = files.FirstOrDefault();
+            DropZone.Visibility = Visibility.Hidden;
+        }
+
+        private void Window_DragEnter(object sender, DragEventArgs e)
+        {
+            DropZone.Visibility = Visibility.Visible;
+        }
+
+        private void Window_DragLeave(object sender, DragEventArgs e)
+        {
+            DropZone.Visibility = Visibility.Hidden;
+        }
+
+        private void CompareHash()
+        {
+            if (!string.IsNullOrEmpty(SourceHash))
+            {
+                if (Hash.ToLower() == SourceHash.ToLower().Trim())
+                {
+                    ComparisonResult.Text = "Hash match";
+                    ComparisonResult.Foreground = Brushes.Green;
+                }
+                else
+                {
+                    ComparisonResult.Text = "Hash mismatch";
+                    ComparisonResult.Foreground = Brushes.Red;
+                }
+            }
+        }
+
+        private void ComparisonResult_KeyUp(object sender, KeyEventArgs e)
+        {
+            CompareHash();
+        }
+    }
+}
